acd6d9c40ec4788e404034ea1f9a8bfdbeef52e7
boopathytcu@gmail.com <boo03061988>
2015-03-16 23:10:44 +0530

    RainbowMobileSuggestion -Mock app

---
+++ RainbowMobileUserSuggestion/src/com/rainbowmobiles/suggest/SplashActivity.java
@@ -0,0 +1,49 @@
+package com.rainbowmobiles.suggest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash_screen);
+		 Thread background = new Thread() {
+	            public void run() {
+	                 
+	                try {
+	                    // Thread will sleep for 5 seconds
+	                    sleep(5*1000);
+	              // int sleep=ApplicationConstants.SPLASH_TIME;
+	                    Intent invokeToMainActivity = new Intent(
+	    						getApplicationContext(), MainActivity.class);
+
+	    				startActivity(invokeToMainActivity);
+	                     
+	                    //Remove activity
+	                    finish();
+	                     
+	                } catch (Exception e) {
+	                 
+	                }
+	            }
+	        };
+	         
+	        // start thread
+	        background.start();
+		
+		/*new Handler().postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				
+				Intent invokeToMainActivity = new Intent(
+						getApplicationContext(), MainActivity.class);
+
+				startActivity(invokeToMainActivity);
+			}
+		}, ApplicationConstants.SPLASH_TIME);*/
+	}
+}
---
+++ RainbowMobileUserSuggestion/src/com/rainbowmobiles/suggest/SuccessActivity.java
@@ -0,0 +1,42 @@
+package com.rainbowmobiles.suggest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+
+public class SuccessActivity extends Activity {
+
+	ImageButton close;
+	RelativeLayout successLayout;
+	Animation animation;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.success);
+		initWidgets();
+		animation = AnimationUtils.loadAnimation(getApplicationContext(),
+				R.anim.slide_in_from_top);
+		successLayout.startAnimation(animation);
+
+	}
+
+	/**
+	 * 
+	 */
+	private void initWidgets() {
+		close = (ImageButton) findViewById(R.id.close);
+		successLayout=(RelativeLayout) findViewById(R.id.success_screen);
+		close.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				finish();
+				System.exit(0);
+			}
+		});
+	}
+}
